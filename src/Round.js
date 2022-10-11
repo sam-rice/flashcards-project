bfedd68da2b11a5f8875ca1a5c603d74456cbd23
Sam Rice <samsrice@gmail.com>
2022-10-10 22:27:04 -0700

    preserve array-index method of updating currentCard

--- src/Round.js
+++ src/Round.js
@@ -4,19 +4,24 @@ class Round {
   constructor(deck) {
     this.deck = deck
     this.turnCount = 0
-    this.currentCard = this.deck.cards[this.turnCount]
+    this.cardIndex = 0
+    this.currentCard = this.deck.cards[this.cardIndex]
     this.incorrectGuesses = []
   };
   
   returnCurrentCard = () => this.currentCard;
   takeTurn = (guess) => {
-    this.currentTurn = new Turn(guess, this.currentCard);
     this.turnCount++;
+
+    this.currentTurn = new Turn(guess, this.currentCard);
+
     if (!this.currentTurn.evaluateGuess()) {
       this.incorrectGuesses.push(this.currentTurn.guess);
     };
+    this.currentCard = this.deck.cards[this.cardIndex + 1]
     return this.currentTurn.giveFeedback();
   };
+  calculatePercentCorrect = () => ((this.turnCount - this.incorrectGuesses.length) / this.turnCount) * 100;
 
 };
 
--- test/Round-test.js
+++ test/Round-test.js
@@ -19,7 +19,7 @@ describe("Round", function() {
     expect(round.currentCard).to.deep.equal(card1);
   });
 
-  it("should have the method returnCurrentCard(), which returns the current card being played", function() {
+  it("should have a method that returns the current card being played", function() {
     let card1 = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
     let card2 = new Card(2, "What year was the Gibson Les Paul released?", ["1951", "1952", "1958", "1963"], "1952");
     let card3 = new Card(3, "What year was the Epiphone Sheraton released?", ["1951", "1966", "1958", "1959"], "1959");
@@ -30,7 +30,7 @@ describe("Round", function() {
     expect(round.returnCurrentCard()).to.deep.equal(round.currentCard);
   });
 
-  it("should have the method takeTurn(), which creates a new instance of Turn, updates the turn count, adds incorrect answers to an incorrectGuesses property, and returns feedback on guess", function() {
+  it("should have a method that creates a new instance of Turn, updates the turn count, adds incorrect answers to an incorrectGuesses property, and returns feedback on guess", function() {
     let card1 = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
     let card2 = new Card(2, "What year was the Gibson Les Paul released?", ["1951", "1952", "1958", "1963"], "1952");
     let card3 = new Card(3, "What year was the Epiphone Sheraton released?", ["1951", "1966", "1958", "1959"], "1959");
@@ -46,6 +46,28 @@ describe("Round", function() {
     expect(round.takeTurn("1963")).to.equal("incorrect!");
     expect(round.turnCount).to.equal(2);
     expect(round.incorrectGuesses).to.deep.equal(["1963"]);
+  });
+
+  it("should have the method that calculates and returns the percentage of correct guesses", function() {
+    let card1 = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
+    let card2 = new Card(2, "What year was the Gibson Les Paul released?", ["1951", "1952", "1958", "1963"], "1952");
+    let card3 = new Card(3, "What year was the Epiphone Sheraton released?", ["1951", "1966", "1958", "1959"], "1959");
+    let card4 = new Card(4, "What year was the Fender Precision Bass released?", ["1950", "1963", "1958", "1951"], "1951");
+    let deck = new Deck([card1, card2, card3, card4]);
+    let round = new Round(deck);
+    console.log(round)
+    round.takeTurn("1951");
+// console.log(round)
+    // expect(round.calculatePercentCorrect()).to.equal(0);
+
+    round.takeTurn("1952");
+// console.log(round)
+    // expect(round.calculatePercentCorrect()).to.equal(50);
+
+    round.takeTurn("1966");
+    round.takeTurn("1950");
+
+    expect(round.calculatePercentCorrect()).to.equal(25);
   });
 
 });
