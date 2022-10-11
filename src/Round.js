6a027518f85f7299eb6b119e27fa5ed357837fdd
Sam Rice <samsrice@gmail.com>
2022-10-10 22:36:42 -0700

    fix takeTurn() currentCard-updating bug

--- src/Round.js
+++ src/Round.js
@@ -4,23 +4,22 @@ class Round {
   constructor(deck) {
     this.deck = deck
     this.turnCount = 0
-    this.cardIndex = 0
-    this.currentCard = this.deck.cards[this.cardIndex]
+    this.currentCard = this.deck.cards[this.turnCount]
     this.incorrectGuesses = []
   };
   
   returnCurrentCard = () => this.currentCard;
+
   takeTurn = (guess) => {
     this.turnCount++;
-
     this.currentTurn = new Turn(guess, this.currentCard);
-
     if (!this.currentTurn.evaluateGuess()) {
       this.incorrectGuesses.push(this.currentTurn.guess);
     };
-    this.currentCard = this.deck.cards[this.cardIndex + 1]
+    this.currentCard = this.deck.cards[this.turnCount]
     return this.currentTurn.giveFeedback();
   };
+  
   calculatePercentCorrect = () => ((this.turnCount - this.incorrectGuesses.length) / this.turnCount) * 100;
 
 };
--- test/Round-test.js
+++ test/Round-test.js
@@ -48,25 +48,20 @@ describe("Round", function() {
     expect(round.incorrectGuesses).to.deep.equal(["1963"]);
   });
 
-  it("should have the method that calculates and returns the percentage of correct guesses", function() {
+  it("should have a method that calculates and returns the percentage of correct guesses", function() {
     let card1 = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
     let card2 = new Card(2, "What year was the Gibson Les Paul released?", ["1951", "1952", "1958", "1963"], "1952");
     let card3 = new Card(3, "What year was the Epiphone Sheraton released?", ["1951", "1966", "1958", "1959"], "1959");
     let card4 = new Card(4, "What year was the Fender Precision Bass released?", ["1950", "1963", "1958", "1951"], "1951");
     let deck = new Deck([card1, card2, card3, card4]);
     let round = new Round(deck);
-    console.log(round)
+
     round.takeTurn("1951");
-// console.log(round)
-    // expect(round.calculatePercentCorrect()).to.equal(0);
-
+    expect(round.calculatePercentCorrect()).to.equal(0);
     round.takeTurn("1952");
-// console.log(round)
-    // expect(round.calculatePercentCorrect()).to.equal(50);
-
+    expect(round.calculatePercentCorrect()).to.equal(50);
     round.takeTurn("1966");
     round.takeTurn("1950");
-
     expect(round.calculatePercentCorrect()).to.equal(25);
   });
 
