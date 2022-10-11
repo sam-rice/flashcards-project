f07678460f237bdadfb0488c45e9296145b25abf
Sam Rice <samsrice@gmail.com>
2022-10-10 21:29:20 -0700

    add returnCurrentCard() and takeTurn() method testing/implementation code

--- src/Round.js
+++ src/Round.js
@@ -1,9 +1,18 @@
+const Turn = require("./Turn");
+
 class Round {
   constructor(deck) {
     this.deck = deck
     this.currentCard = this.deck.cards[0]
+    this.turnCount = 0
   };
   
+  returnCurrentCard = () => this.currentCard;
+  takeTurn = (guess) => {
+    this.currentTurn = new Turn(guess);
+    this.turnCount++;
+  };
+
 };
 
 module.exports = Round;
--- test/Round-test.js
+++ test/Round-test.js
@@ -2,6 +2,7 @@ const chai = require('chai');
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
+const Turn = require('../src/Turn');
 const expect = chai.expect;
 
 describe("Round", function() {
@@ -16,6 +17,31 @@ describe("Round", function() {
 
     expect(round.deck).to.deep.equal(deck);
     expect(round.currentCard).to.deep.equal(card1);
+  });
+
+  it("should have the method returnCurrentCard(), which returns the current card being played", function() {
+    let card1 = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
+    let card2 = new Card(2, "What year was the Gibson Les Paul released?", ["1951", "1952", "1958", "1963"], "1952");
+    let card3 = new Card(3, "What year was the Epiphone Sheraton released?", ["1951", "1966", "1958", "1959"], "1959");
+    let card4 = new Card(4, "What year was the Fender Precision Bass released?", ["1950", "1963", "1958", "1951"], "1951");
+    let deck = new Deck([card1, card2, card3, card4]);
+    let round = new Round(deck);
+
+    expect(round.returnCurrentCard()).to.deep.equal(round.currentCard);
+  });
+
+  it("should have the method takeTurn(), which creates a new instance of Turn and updates the turn count", function() {
+    let card1 = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
+    let card2 = new Card(2, "What year was the Gibson Les Paul released?", ["1951", "1952", "1958", "1963"], "1952");
+    let card3 = new Card(3, "What year was the Epiphone Sheraton released?", ["1951", "1966", "1958", "1959"], "1959");
+    let card4 = new Card(4, "What year was the Fender Precision Bass released?", ["1950", "1963", "1958", "1951"], "1951");
+    let deck = new Deck([card1, card2, card3, card4]);
+    let round = new Round(deck);
+
+    round.takeTurn("1958");
+
+    expect(round.currentTurn).to.be.an.instanceOf(Turn);
+    expect(round.turnCount).to.equal(1);
   });
 
 });
