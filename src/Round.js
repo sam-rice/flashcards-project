78a660dc7eaa084c7536b91e002269211be1f88d
Sam Rice <samsrice@gmail.com>
2022-10-10 21:57:23 -0700

    add complete implementation/testing code to takeTurn()

--- src/Round.js
+++ src/Round.js
@@ -3,14 +3,19 @@ const Turn = require("./Turn");
 class Round {
   constructor(deck) {
     this.deck = deck
-    this.currentCard = this.deck.cards[0]
     this.turnCount = 0
+    this.currentCard = this.deck.cards[this.turnCount]
+    this.incorrectGuesses = []
   };
   
   returnCurrentCard = () => this.currentCard;
   takeTurn = (guess) => {
-    this.currentTurn = new Turn(guess);
+    this.currentTurn = new Turn(guess, this.currentCard);
     this.turnCount++;
+    if (!this.currentTurn.evaluateGuess()) {
+      this.incorrectGuesses.push(this.currentTurn.guess);
+    };
+    return this.currentTurn.giveFeedback();
   };
 
 };
--- test/Round-test.js
+++ test/Round-test.js
@@ -30,7 +30,7 @@ describe("Round", function() {
     expect(round.returnCurrentCard()).to.deep.equal(round.currentCard);
   });
 
-  it("should have the method takeTurn(), which creates a new instance of Turn and updates the turn count", function() {
+  it("should have the method takeTurn(), which creates a new instance of Turn, updates the turn count, adds incorrect answers to an incorrectGuesses property, and returns feedback on guess", function() {
     let card1 = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
     let card2 = new Card(2, "What year was the Gibson Les Paul released?", ["1951", "1952", "1958", "1963"], "1952");
     let card3 = new Card(3, "What year was the Epiphone Sheraton released?", ["1951", "1966", "1958", "1959"], "1959");
@@ -38,10 +38,14 @@ describe("Round", function() {
     let deck = new Deck([card1, card2, card3, card4]);
     let round = new Round(deck);
 
-    round.takeTurn("1958");
-
+    expect(round.takeTurn("1958")).to.equal("correct!")
     expect(round.currentTurn).to.be.an.instanceOf(Turn);
     expect(round.turnCount).to.equal(1);
+    expect(round.incorrectGuesses.length).to.equal(0);
+
+    expect(round.takeTurn("1963")).to.equal("incorrect!");
+    expect(round.turnCount).to.equal(2);
+    expect(round.incorrectGuesses).to.deep.equal(["1963"]);
   });
 
 });
