21142c648dc3dc8dcc074e2d826e529925fcb429
Sam Rice <samsrice@gmail.com>
2022-10-11 18:30:11 -0700

    refactor Turn-test.js

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -14,7 +14,7 @@ describe("Turn", function() {
   });
 
   it("should be instantiated with two arguments", function() {
-    let card = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
+    let card = new Card(data.prototypeData[0]);
     let turn = new Turn("1958", card);
     
     expect(turn.guess).to.equal("1958");
@@ -22,32 +22,32 @@ describe("Turn", function() {
   })
 
   it("should have a method that returns the guess", function() {
-    let card = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
+    let card = new Card(data.prototypeData[0]);
     let turn = new Turn("1958", card);
     
     expect(turn.returnGuess()).to.equal(turn.guess);
   });
 
   it("should have a method that returns the card", function() {
-    let card = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
+    let card = new Card(data.prototypeData[0]);
     let turn = new Turn("1958", card);
     
     expect(turn.returnCard()).to.deep.equal(turn.card);
   });
 
   it("should have a method that returns a boolean indicating if the user’s guess matches the correct answer on the card", function() {
-    let card = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
-    let correctTurn = new Turn("1958", card);
-    let incorrectTurn = new Turn("1951", card);
+    let card = new Card(data.prototypeData[0]);
+    let correctTurn = new Turn("object", card);
+    let incorrectTurn = new Turn("array", card);
     
     expect(correctTurn.evaluateGuess()).to.equal(true);
     expect(incorrectTurn.evaluateGuess()).to.equal(false);
   });
 
   it("should have a method that returns either ‘incorrect!’ or ‘correct!’ based on whether the guess is correct or not", function() {
-    let card = new Card(1, "What year was the Fender Jazzmaster released?", ["1951", "1963", "1958", "1970"], "1958");
-    let correctTurn = new Turn("1958", card);
-    let incorrectTurn = new Turn("1951", card);
+    let card = new Card(data.prototypeData[0]);
+    let correctTurn = new Turn("object", card);
+    let incorrectTurn = new Turn("array", card);
     
     expect(correctTurn.giveFeedback()).to.equal("correct!");
     expect(incorrectTurn.giveFeedback()).to.equal("incorrect!");
