9362efa9e98faf3af5ff3ee85c6e6dcd8f1556bb
Sam Rice <samsrice@gmail.com>
2022-10-13 13:30:38 -0700

    update Deck-test.js to use testing dataset

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
-const data = require('../src/data');
+const data = require('../src/test-data');
 
 
 describe("Deck", function() {
