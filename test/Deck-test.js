f96f032ad917f3d9a7f38ef51b471cc8a1992f18
Sam Rice <samsrice@gmail.com>
2022-10-13 12:27:17 -0700

    implement forEach() in Deck-test.js

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -6,30 +6,26 @@ const data = require('../src/data');
 
 
 describe("Deck", function() {
+  let cards;
+  let deck;
+
+  this.beforeEach(() => {
+    cards = [];
+    for (let i = 0; i < 4; i++) { 
+      cards.push(new Card(data.prototypeData[i]));
+    };
+    deck = new Deck(cards);
+  });
 
   it("should be an instance of Deck", function() {
-    let deck = new Deck();
-
     expect(deck).to.be.an.instanceOf(Deck);
   });
 
   it("should be instantiated with an array of Cards", function() {
-    let cards = []
-    for (let i = 0; i < 4; i++) { 
-      cards.push(new Card(data.prototypeData[i]));
-    };
-    let deck = new Deck(cards);
-
     expect(deck.cards).to.deep.equal(cards);
   });
 
   it("should know how many Cards are in the Deck", function() {
-    let cards = []
-    for (let i = 0; i < 4; i++) { 
-      cards.push(new Card(data.prototypeData[i]));
-    };
-    let deck = new Deck(cards);
-
     expect(deck.deckLength).to.equal(4);
   });
 });
