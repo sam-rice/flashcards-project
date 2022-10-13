de74b160133fbd7a36bf008719aaebd2f70c93ad
Sam Rice <samsrice@gmail.com>
2022-10-13 12:19:26 -0700

    implement beforeEach() in Card-test.js

--- test/Card-test.js
+++ test/Card-test.js
@@ -5,24 +5,23 @@ const Card = require('../src/Card');
 const data = require('../src/data');
 
 describe('Card', function() {
+  let card;
+
+  this.beforeEach(() => card = new Card(data.prototypeData[0]));
 
   it('should be an instance of Card', function() {
-    const card = new Card({});
     expect(card).to.be.an.instanceof(Card);
   }); 
 
   it('should store a question', function() {
-    const card = new Card(data.prototypeData[0]);
     expect(card.question).to.equal('What allows you to define a set of related information using key-value pairs?');
   });  
 
   it('should store a list of possible answers', function() {
-    const card = new Card(data.prototypeData[0]);
     expect(card.answers).to.deep.equal(['object', 'array', 'function']);
   });  
 
   it('should store the correct answer', function() {
-    const card = new Card(data.prototypeData[0]);
     expect(card.correctAnswer).to.equal('object');
   });
 });
