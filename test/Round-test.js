147772247b39a3634737fec2b6804c00a1c15303
Sam Rice <samsrice@gmail.com>
2022-10-13 13:38:12 -0700

    update Round-test.js to use testing dataset

--- test/Round-test.js
+++ test/Round-test.js
@@ -4,7 +4,7 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Turn = require('../src/Turn');
-const data = require('../src/data');
+const data = require('../src/test-data');
 
 describe("Round", function() {
   let cards, deck, round;
@@ -29,25 +29,25 @@ describe("Round", function() {
 
   it("should have a method that creates a new instance of Turn, updates the turn count/current Card, adds incorrect answers to an array, and returns feedback on guess", function() {
     expect(round.currentCard).to.deep.equal(cards[0])
-    expect(round.takeTurn("object")).to.equal("correct!")
+    expect(round.takeTurn("1958")).to.equal("correct!")
     expect(round.currentTurn).to.be.an.instanceOf(Turn);
     expect(round.turnCount).to.equal(1);
     expect(round.incorrectGuesses.length).to.equal(0);
     expect(round.currentCard).to.deep.equal(cards[1])
 
-    expect(round.takeTurn("function")).to.equal("incorrect!");
+    expect(round.takeTurn("1963")).to.equal("incorrect!");
     expect(round.turnCount).to.equal(2);
-    expect(round.incorrectGuesses).to.deep.equal(["function"]);
+    expect(round.incorrectGuesses).to.deep.equal(["1963"]);
     expect(round.currentCard).to.deep.equal(cards[2])
   });
 
   it("should have a method that calculates and returns the percentage of correct guesses", function() {
-    round.takeTurn("function");
+    round.takeTurn("1951");
     expect(round.calculatePercentCorrect()).to.equal(0);
-    round.takeTurn("array");
+    round.takeTurn("1952");
     expect(round.calculatePercentCorrect()).to.equal(50);
-    round.takeTurn("accessor method");
-    round.takeTurn("mutator method");
+    round.takeTurn("1966");
+    round.takeTurn("1963");
     expect(round.calculatePercentCorrect()).to.equal(25);
   });
 
